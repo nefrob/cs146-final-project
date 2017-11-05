945755e80af16fc8ea9cca1cbf9016d182b1834b
nefrob <25070989+nefrob@users.noreply.github.com>
2017-11-05 11:23:03 -0800

    Added moving platform prefab

--- CS146_Final_Project/Assets/Scripts/MovingPlatform.cs
+++ CS146_Final_Project/Assets/Scripts/MovingPlatform.cs
@@ -16,6 +16,8 @@ public class MovingPlatform : MonoBehaviour {
     [SerializeField] private bool useY = false;
     [SerializeField] private float time = 1.0f;
     [SerializeField] private float distance = 10.0f;
+    //
+    private Transform playerParent;
 
     // Start looped movement
     void Start () {
@@ -25,5 +27,16 @@ public class MovingPlatform : MonoBehaviour {
             "easeType", "easeInOutExpo", "time", time, "looptype", "pingpong"));
         else iTween.MoveBy(gameObject, iTween.Hash("z", distance, 
             "easeType", "easeInOutExpo", "time", time, "looptype", "pingpong"));
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        playerParent = collision.gameObject.transform.parent;
+        collision.gameObject.transform.parent = gameObject.transform;
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        collision.gameObject.transform.parent = playerParent;
     }
 }
