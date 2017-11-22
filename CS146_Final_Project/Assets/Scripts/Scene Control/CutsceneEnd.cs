3b87fa7163f8c65a105da25ef2a51662e86a9be3
nefrob <25070989+nefrob@users.noreply.github.com>
2017-11-22 12:14:03 -0800

    Organization and small changes
    Fixed explosion delay problem!!! :)

---
+++ CS146_Final_Project/Assets/Scripts/Scene Control/CutsceneEnd.cs
@@ -0,0 +1,17 @@
+﻿/*
+* File:        Cut Scene End
+* Author:      Robert Neff
+* Date:        11/22/17
+* Description: Loads the first level of the game after cutscene.
+*/
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CutsceneEnd : MonoBehaviour {
+
+    public void loadLevelOne()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1); // first level of game
+    }
+}
